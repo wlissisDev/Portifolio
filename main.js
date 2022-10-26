c656001975aea45a5c2b49bb7f512f6e2ce36454
wlissisdev <wlissisrds@outlook.com>
2022-10-26 14:40:13 -0300

    portifolio

---
+++ main.js
@@ -0,0 +1,30 @@
+// const listaProjetos = document.querySelector(".lista-projetos");
+
+// function createProjeto(pathImage) {
+//   const li = document.createElement("li");
+//   const a = document.createElement("a");
+
+//   li.style = `
+//   width: 300px;
+//   height: 300px;
+//   background: url("images/projetosimage/projeto0${pathImage}.png") no-repeat;
+//   background-size: 450px;
+//   background-position: center;
+//   `;
+
+//   a.innerHTML = "VER";
+//   a.setAttribute("href", "#");
+//   li.appendChild(a);
+//   li.classList.add("projeto");
+
+//   listaProjetos.appendChild(li);
+// }
+
+// // createProjeto(1)
+// function addProjetos() {
+//   for (let i = 1; i <= 2; i++) {
+//     createProjeto(i);
+//   }
+// }
+
+// addProjetos();
